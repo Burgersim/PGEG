9786a25c455ccbe3f8f6ad9e8f2dfdae5cf246c4
Mika <salzer.michhael@gmx.net>
2018-07-25 20:14:28 +0200

    first try

--- src/main/java/org/burgersim/pgeg/MagicTree.java
+++ src/main/java/org/burgersim/pgeg/MagicTree.java
@@ -13,14 +13,14 @@ public class MagicTree implements BlockAdder, ItemAdder {
     Block MAGIC_OAK_LOG = new BlockLog(MapColor.COLORS[0x46567c], Block.Builder.create(Material.WOOD));
     Block MAGIC_OAK_PLANKS = new Block(Block.Builder.create(Material.WOOD, MapColor.COLORS[0x55668f]));
     Block MAGIC_OAK_LEAVES = new BlockLeaves(Block.Builder.create(Material.LEAVES, MapColor.COLORS[0xecc67a]));
-    Block MAGIC_DOOR = new BlockDoor(Block.Builder.create(Material.WOOD, MapColor.COLORS[0x55668f]));
+//    Block MAGIC_DOOR = new BlockDoor(Block.Builder.create(Material.WOOD, MapColor.COLORS[0x55668f]));
 
     @Override
     public void registerBlocks() {
         Block.registerBlock(new ResourceLocation("pgeg:magic_oak_log"), MAGIC_OAK_LOG);
         Block.registerBlock(new ResourceLocation("pgeg:magic_oak_planks"), MAGIC_OAK_PLANKS);
         Block.registerBlock(new ResourceLocation("pgeg:magic_oak_leaves"), MAGIC_OAK_LEAVES);
-        Block.registerBlock(new ResourceLocation("pgeg:magic_door"), MAGIC_DOOR);
+//        Block.registerBlock(new ResourceLocation("pgeg:magic_door"), MAGIC_DOOR);
 
     }
 
