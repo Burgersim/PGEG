4d0a4212dd4ccd8ae1e95a571ee0e5635ef06a90
Burgersim <salzer.michael@gmx.net>
2018-08-25 20:24:05 +0200

    Mining Levels

--- src/main/java/org/burgersim/pgeg/listener/PgegMiningLevel.java
+++ src/main/java/org/burgersim/pgeg/listener/PgegMiningLevel.java
@@ -2,12 +2,20 @@ package org.burgersim.pgeg.listener;
 
 import com.github.ondee.snowflake.listener.MiningLevelAdder;
 import net.minecraft.block.Block;
-
 import java.util.Map;
+
+import static net.minecraft.init.Blocks.DIAMOND_ORE;
+
 
 public class PgegMiningLevel implements MiningLevelAdder {
     @Override
     public void addMiningLevel(Map<Block, Integer> map) {
         map.put(PgegRegistry.MANA_CRYSTAL_ORE, 2);
+        map.put(PgegRegistry.BRIMSTONE_ORE, 4);
+        map.put(PgegRegistry.MITHRIL_ORE, 4);
+        map.put(PgegRegistry.SATURNIUM_ORE, 5);
+        map.put(PgegRegistry.METEORITE_ORE, 6);
+        map.put(PgegRegistry.ORICHALCUM_ORE, 3);
+        map.put(DIAMOND_ORE, 3);
     }
 }
