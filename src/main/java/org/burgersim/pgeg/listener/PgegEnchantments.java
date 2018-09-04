cd61e89a6f1362024382601371b3366d41ed952a
onDee <volad09@gmail.com>
2018-09-04 21:59:36 +0300

    Added Poisonous enchantment

--- src/main/java/org/burgersim/pgeg/listener/PgegEnchantments.java
+++ src/main/java/org/burgersim/pgeg/listener/PgegEnchantments.java
@@ -2,6 +2,7 @@ package org.burgersim.pgeg.listener;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.ResourceLocation;
+import org.burgersim.pgeg.enchantment.EnchantmentPoisonous;
 import org.burgersim.pgeg.enchantment.EnchantmentVampirism;
 import org.dimdev.rift.listener.EnchantmentAdder;
 
@@ -9,10 +10,12 @@ import static org.burgersim.pgeg.utils.Reference.MOD_ID;
 
 public class PgegEnchantments implements EnchantmentAdder {
     public static Enchantment VAMPIRISM;
+    public static Enchantment POISONOUS;
 
     @Override
     public void registerEnchantments() {
         VAMPIRISM = registerEnchantment(new ResourceLocation(MOD_ID, "vampirism"), new EnchantmentVampirism());
+        POISONOUS = registerEnchantment(new ResourceLocation(MOD_ID, "poisonous"), new EnchantmentPoisonous());
     }
 
     private Enchantment registerEnchantment(ResourceLocation location, Enchantment enchantment) {
--- src/main/java/org/burgersim/pgeg/mixin/common/MixinEntityDamaged.java
+++ src/main/java/org/burgersim/pgeg/mixin/common/MixinEntityDamaged.java
@@ -1,11 +1,18 @@
 package org.burgersim.pgeg.mixin.common;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.PotionTypes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionType;
 import net.minecraft.util.DamageSource;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,6 +23,13 @@ import static org.burgersim.pgeg.utils.Reference.MOD_ID;
 @Mixin(EntityLivingBase.class)
 public abstract class MixinEntityDamaged {
 
+    @Shadow
+    public abstract boolean addPotionEffect(PotionEffect p_addPotionEffect_1_);
+
+    @Shadow
+    @Final
+    private static DataParameter<Integer> POTION_EFFECTS;
+
     @Inject(method = "damageEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;setHealth(F)V"),
             locals = LocalCapture.CAPTURE_FAILEXCEPTION)
     private void onDamageEntity(DamageSource source, float damage, CallbackInfo ci, float finalDamage) {
@@ -25,13 +39,18 @@ public abstract class MixinEntityDamaged {
             if (stack.isItemEnchanted()) {
                 NBTTagList enchantments = stack.getEnchantmentTagList();
                 int vampirismLevel = 0;
+                int poisonousLevel = 0;
                 for (int i = 0; i < enchantments.size(); i++) {
                     NBTTagCompound compound = enchantments.getCompoundTagAt(i);
                     if ((MOD_ID + ":vampirism").equals(compound.getString("id"))) {
                         vampirismLevel = compound.getInteger("lvl");
                     }
+                    if ((MOD_ID + ":poisonous").equals(compound.getString("id"))) {
+                        poisonousLevel = compound.getInteger("lvl");
+                    }
                 }
                 trueSource.setHealth(trueSource.getHealth() + finalDamage * 0.01f * vampirismLevel);
+                this.addPotionEffect(new PotionEffect(MobEffects.POISON, 5 * 20 * poisonousLevel));
             }
         }
     }
